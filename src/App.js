1db04e88106ad4932166471d8db5d75236cb83ba
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-06 14:57:20 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { useState } from 'react';
 import { Calendar } from 'primereact/calendar';
+import NavBar from './components/NavBar';
 
 function App() {
   const [date1, setDate1] = useState(null);
   const [date2, setDate2] = useState(null);
+
   return (
-    <div className="App">
+    <>
+      <NavBar />
       <Calendar value={date1} onChange={(e) => setDate1(e.value)} showIcon/>
       <Calendar value={date2} onChange={(e) => setDate2(e.value)} showIcon/>
-    </div>
+    </>
   );
 }
 
