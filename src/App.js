ddbbf98bbcd4d8df65baa59d071bcc75f23c1f0e
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-06 14:22:27 +0300

    add date 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import 'primereact/resources/themes/lara-light-indigo/themes.css';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import { useState } from 'react';
+import { Calendar } from 'primereact/calendar';
 
 function App() {
+  const [date1, setDate1] = useState(null);
+  const [date2, setDate2] = useState(null);
   return (
     <div className="App">
-      <h1>Primereact</h1>
+      <Calendar value={date1} onChange={(e) => setDate1(e.value)} showIcon/>
+      <Calendar value={date2} onChange={(e) => setDate2(e.value)} showIcon/>
     </div>
   );
 }
