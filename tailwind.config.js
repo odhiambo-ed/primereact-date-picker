05b77e67795c013945332648f2da2e806d1f0e92
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-06 14:32:28 +0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    extend: {},
+  },
+  plugins: [],
+}
+
